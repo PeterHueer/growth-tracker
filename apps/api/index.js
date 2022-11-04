370e4ea92c2f7b2eb51e194ce4931562fa488206
Peter Hueer <peter.huer@unicepta.com>
2022-11-04 16:18:37 +0100

    Add Apollo

---
+++ apps/api/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+exports.__esModule = true;
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+var apollo_server_1 = require("apollo-server");
+var typeDefs = (0, apollo_server_1.gql)(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.\n\n  # This \"Book\" type defines the queryable fields for every book in our data source.\n  type Book {\n    title: String\n    author: String\n  }\n\n  # The \"Query\" type is special: it lists all of the available queries that\n  # clients can execute, along with the return type for each. In this\n  # case, the \"books\" query returns an array of zero or more Books (defined above).\n  type Query {\n    books: [Book]\n  }\n"], ["\n  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.\n\n  # This \"Book\" type defines the queryable fields for every book in our data source.\n  type Book {\n    title: String\n    author: String\n  }\n\n  # The \"Query\" type is special: it lists all of the available queries that\n  # clients can execute, along with the return type for each. In this\n  # case, the \"books\" query returns an array of zero or more Books (defined above).\n  type Query {\n    books: [Book]\n  }\n"])));
+var ApolloServerPluginLandingPageLocalDefault = require("apollo-server-core").ApolloServerPluginLandingPageLocalDefault;
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+var server = new apollo_server_1.ApolloServer({
+    typeDefs: typeDefs,
+    mocks: true,
+    csrfPrevention: true,
+    cache: "bounded",
+    /**
+     * What's up with this embed: true option?
+     * These are our recommended settings for using AS;
+     * they aren't the defaults in AS3 for backwards-compatibility reasons but
+     * will be the defaults in AS4. For production environments, use
+     * ApolloServerPluginLandingPageProductionDefault instead.
+     **/
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })]
+});
+// The `listen` method launches a web server.
+server.listen().then(function (_a) {
+    var url = _a.url;
+    console.log("\uD83D\uDE80  Server ready at ".concat(url));
+});
+var templateObject_1;
